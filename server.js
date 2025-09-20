8c02ae7b7b663496a341d0997e92556193e4b481
ZISHUN GAO <144342588+ZishunD@users.noreply.github.com>
2025-09-20 13:34:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { graphqlUploadExpress } from "graphql-upload";
 import typeDefs from "./schema/index.js";
 import resolvers from "./resolvers/index.js";
-import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config();
+const app = express();
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+app.use(cors({
+    origin: "http://localhost:3000", // 前端地址
+    credentials: true,
+}));
+
+// graphql-upload 中间件
+app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
+
+// Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+server.applyMiddleware({
+    app, cors: {
+        origin: "http://localhost:3000, https://intern-plus-frontend.onrender.com", // 前端地址
+        credentials: true,
+    }
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: { port: process.env.PORT || 4000 },
+// 启动 Express
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
-
-console.log(`🚀 Server ready at ${url}`);
